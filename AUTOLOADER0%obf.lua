1a3c028f6ab31c648f45e1f93795e39a1f18a27b
WolfREK <83781554+WolfREK@users.noreply.github.com>
2021-06-12 01:06:21 +0700

    Rename AUTOLOADER/bypassed/0%obf.lua to AUTOLOADER0%obf.lua

---
+++ AUTOLOADER0%obf.lua
@@ -0,0 +1,6 @@
+--Stop snooping weirdo
+--Omega Hub is hotter than ur uncle
+
+if game.PlaceId == 3956818381 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WolfREK/OmegaHub/main/NinjaLegends20%obf.lua"))()
+end
