2c706d09a9662abc74aff2c9d8312a9d4290c55c
WolfREK <83781554+WolfREK@users.noreply.github.com>
2021-06-12 03:01:43 +0700

    Update AUTOLOADER0%obf.lua

--- AUTOLOADER0%obf.lua
+++ AUTOLOADER0%obf.lua
@@ -2,5 +2,5 @@
 --Omega Hub is hotter than ur uncle
 
 if game.PlaceId == 3956818381 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/WolfREK/OmegaHub/main/NinjaLegends20%obf.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WolfREK/OmegaHub/main/NinjaLegends20%25obf.lua))()
 end
