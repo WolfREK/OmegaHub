004b1ac9b0c1ff93097a8399094111fd0c75d938
WolfREK <83781554+WolfREK@users.noreply.github.com>
2021-06-12 03:05:48 +0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/WolfREK/OmegaHub/main/AUTOLOADER0%25obf.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WolfREK/OmegaHub/main/AUTOLOADER.lua"))()
